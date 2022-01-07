9210fa1fff8f6def0848922eb8225610b813ba6b
Roma <65kgm9ca@gmail.com>
2022-01-07 21:20:41 +0300

    cbv for mainapp, not finished

--- blog/mainapp/urls.py
+++ blog/mainapp/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 from . import views
-
+from .models import Post
 
 app_name = 'posts'
 urlpatterns = [
-    path('', views.posts_list, name='posts_list'),
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('drafts/', views.posts_drafts_list, name='posts_drafts_list'),
-    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    # path('', views.posts_list, name='posts_list'),
+    path('', views.PostsListView.as_view(), name='posts_list'),
+    # path('post/new/', views.post_new, name='post_new'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_new'),
+    # path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    # path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    # path('drafts/', views.posts_drafts_list, name='posts_drafts_list'),
+    path('drafts/', views.PostsDraftsListView.as_view(), name='posts_drafts_list'),
+    # path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/publish/', views.PostUpdateView.post_publish, name='post_publish'),
     path('post/<pk>/delete/', views.post_delete, name='post_delete'),
 ]
--- blog/mainapp/views.py
+++ blog/mainapp/views.py
@@ -1,58 +1,106 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
 from django.utils import timezone
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.utils.translation import gettext as _
 from .forms import PostForm
+from .mixin import BaseClassContextMixin, CustomDispatchMixin, UserDispatchMixin
 from .models import Post
 
 
 # Create your views here.
-def posts_list(request):
-    # __lte = less than equal
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'mainapp/posts_list.html', {'posts': posts})
+# def posts_list(request):
+#     # __lte = less than equal
+#     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+#     return render(request, 'mainapp/posts_list.html', {'posts': posts})
 
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'mainapp/post_detail.html', {'post': post})
+class PostsListView(ListView, BaseClassContextMixin):
+    model = Post
+    template_name = 'mainapp/posts_list.html'
+    title = _('Posts list')
+
+    def get_queryset(self):
+        # __lte = less than equal
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+        return posts
 
 
-@login_required
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            # post.published_date = timezone.now()
-            post.save()
-            return redirect('mainapp:post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'mainapp/post_edit.html', {'form': form})
+class PostDetailView(DetailView, BaseClassContextMixin):
+    model = Post
+    title = _('Post view')
 
 
-@login_required
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('mainapp:post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'mainapp/post_edit.html', {'form': form})
+class PostCreateView(CreateView, BaseClassContextMixin, UserDispatchMixin):
+    model = Post
+    title = _('Post create')
+    template_name = 'mainapp/post_edit.html'
+    form_class = PostForm
+    success_url = 'mainapp:post_detail'
+
+    def form_valid(self, form):
+        """
+        if form is valid fill author post with current user
+        @param form:
+        @type form:
+        @return:
+        @rtype:
+        """
+        form.instance.author = self.request.user
+        return super(PostCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        """
+        if success redirect to created post view
+        @return:
+        @rtype:
+        """
+        return reverse(self.success_url, args=(self.object.id,))
 
 
-@login_required
-def posts_drafts_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_at')
-    return render(request, 'mainapp/posts_drafts_list.html', {'posts': posts})
+class PostUpdateView(UpdateView, BaseClassContextMixin, UserDispatchMixin):
+    model = Post
+    title = _('Post edit')
+    template_name = 'mainapp/post_edit.html'
+    form_class = PostForm
+    success_url = 'mainapp:post_detail'
+
+    def get_success_url(self):
+        """
+        if success redirect to created post view
+        @return:
+        @rtype:
+        """
+        return reverse(self.success_url, args=(self.object.id,))
+
+    def post_publish(self, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        post.publish()
+        # self.object = self.get_object()
+        # self.object.publish()
+        return redirect('mainapp:post_detail', pk=kwargs['pk'])
+        # return HttpResponseRedirect(self.get_success_url())
+        # return redirect(self.get_success_url())
+        # return reverse(PostUpdateView.success_url, args=(self.object.id,))
+
+    def get_object(self, *args, **kwargs):
+        return get_object_or_404(Post, pk=kwargs['pk'])
+    #
+    # def form_valid(self, form):
+    #     form.instance.customer_id = self.kwargs['pk']
+    #     return super(PostUpdateView, self).form_valid(form)
+
+
+class PostsDraftsListView(ListView, BaseClassContextMixin, UserDispatchMixin):
+    model = Post
+    template_name = 'mainapp/posts_drafts_list.html'
+    title = _('Posts drafts list')
+
+    def get_queryset(self):
+        posts_drafts = Post.objects.filter(published_date__isnull=True).order_by('created_at')
+        return posts_drafts
 
 
 @login_required
